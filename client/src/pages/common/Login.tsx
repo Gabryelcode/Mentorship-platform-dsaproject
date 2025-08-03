6e58b9a192b708632934457b010d1d40611a6090
Ede Gabriel <gabrieledeomam@gmail.com>
2025-08-03 21:33:35 +0100

    Modified the register page and index.ts

--- client/src/pages/common/Login.tsx
+++ client/src/pages/common/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post('/auth/login', { email, password });
       const { token, user } = res.data;
 
       sessionStorage.setItem('token', token);
